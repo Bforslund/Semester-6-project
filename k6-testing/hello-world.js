bb0222f1e7ccbf679f15c78d7ca0b429ef8bb857
Beatrice Forslund <forslund.beatrice@gmail.com>
2022-06-06 19:00:54 +0200

    influxDB

--- k6-testing/hello-world.js
+++ k6-testing/hello-world.js
@@ -1,10 +1,10 @@
 import http from 'k6/http';
 
 export const options = {
-    vus: 100,
+    vus: 250,
     duration: '30s',
   };
 
 export default function(){
-    http.get('http://booking.bea.local:9080/Booking/bookings');
+    http.get('https://hotel.d28c7b7324414a4cba3b.germanywestcentral.aksapp.io/Hotel/hotels');
 }
